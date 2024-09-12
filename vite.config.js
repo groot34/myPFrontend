ac22ad1d77c1fe2d321aa768bac807c05f10b55e
Atharva <121598279+groot34@users.noreply.github.com>
2024-09-12 19:14:29 +0530

    new-chnages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://my-pf-backend.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://my-pf-backend.vercel.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
