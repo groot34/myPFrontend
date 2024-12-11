432cd97838d48ecbb8572b4f5acfc896fcbcc310
Atharva <121598279+groot34@users.noreply.github.com>
2024-12-11 16:29:09 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import TypingEffect from "./TypingEffect";
 import illustration from "../assets/illustration.svg";
+import headerimg from "../assets/header-img.svg";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -21,7 +22,6 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            {/* Heading */}
             <motion.h1
               variants={container(0)}
               initial="hidden"
@@ -32,17 +32,15 @@ const Hero = () => {
               <span className="text-[#FFFDD0] font-semibold">C</span>omrade🫡
             </motion.h1>
 
-      
             <motion.span
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="text-4xl sm:text-5xl lg:text-6xl font-bold   text-white bg-clip-text text-transparent "
+              className="text-3xl sm:text-5xl lg:text-6xl font-bold text-white bg-clip-text text-transparent whitespace-nowrap"
             >
               I'm <span className="italic">Atharva Tikale</span>
             </motion.span>
 
-         
             <motion.p
               variants={container(1)}
               initial="hidden"
@@ -54,20 +52,28 @@ const Hero = () => {
           </div>
         </div>
 
-        {/* Right Section (Image) */}
-        <div className="w-full lg:w-1/2 lg:p-8">
-  <div className="flex justify-center">
-    <motion.img
-      initial={{ x: 100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: 1.2 }}
-      src={illustration}
-      alt="profile pic"
-      className="w-10/12 lg:w-10/12"  // Adjust the width for different screen sizes
-    />
-  </div>
-</div>
-
+        <div className=" w-full lg:w-1/2 lg:p-8">
+          <motion.img
+            initial={{ scale: 0.8, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            viewport={{ once: true }} 
+            animate={{
+              y: [0, -10, 0], 
+            }}
+            transition={{
+              duration: 1, 
+              delay: 0.2, 
+              y: {
+                duration: 2, 
+                repeat: Infinity,
+                repeatType: "loop",
+              },
+            }}
+            src={headerimg}
+            alt="profile pic"
+            className="w-9/12 sm:w-7/12 lg:w-6/12 xl:w-5/12 mx-auto sm:ml-auto"
+          />
+        </div>
       </div>
     </div>
   );
